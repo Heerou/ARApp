65662b040b979bae3363b97c809115603ae98ca1
heerou <jalvarezpimiento@gmail.com>
2020-01-16 21:13:22 -0500

    UI and basic transitions vfx

---
+++ Assets/Assets/Scripts/PresentationScene/Transition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour
+{
+    [SerializeField]
+    Image transitionImg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(FadeTo());
+    }
+
+    IEnumerator FadeTo()
+    {
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            transitionImg.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+}
